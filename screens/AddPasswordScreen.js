934ff66dc1a1c3aaa67e191527ccc67d4acdd900
Dacooleric <dacooleric@gmail.com>
2023-07-22 06:52:45 +0000

    first commit

---
+++ screens/AddPasswordScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const AddPasswordScreen = () => {
+  return (
+    <View>
+      <Text>AddPasswordScreen</Text>
+    </View>
+  );
+};
+
+export default AddPasswordScreen;
+
+const styles = StyleSheet.create({});
